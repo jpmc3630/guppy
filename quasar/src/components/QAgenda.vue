c081ef5b3f650e1bc5ebcf40c31706c79e9eaec8
James McGlone <jamespmcglone@gmail.com>
2022-04-17 00:52:26 +0800

    Qcal boot and components

---
+++ quasar/src/components/QAgenda.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="subcontent full-width">
+    <navigation-bar
+      @today="onToday"
+      @prev="onPrev"
+      @next="onNext"
+    />
+
+    <div class="row justify-center">
+      <div style="display: flex; max-width: 800px; width: 100%; height: 200px;">
+        <q-calendar-agenda
+          ref="calendar"
+          v-model="selectedDate"
+          view="week"
+          :weekdays="[1,2,3,4,5,6,0]"
+          column-options-id="id"
+          column-options-label="label"
+          :day-min-height="200"
+          animated
+          bordered
+          @change="onChange"
+          @moved="onMoved"
+          @click-date="onClickDate"
+          @click-time="onClickTime"
+          @click-interval="onClickInterval"
+          @click-head-intervals="onClickHeadIntervals"
+          @click-head-day="onClickHeadDay"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { QCalendarAgenda, today } from '@quasar/quasar-ui-qcalendar/src/index.js'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarAgenda.sass'
+
+import { defineComponent } from 'vue'
+import NavigationBar from '@/components/NavigationBar.vue'
+
+export default defineComponent({
+  name: 'DayFirstDayMonday',
+  components: {
+    NavigationBar,
+    QCalendarAgenda
+  },
+  data () {
+    return {
+      selectedDate: today(),
+    }
+  },
+  methods: {
+    onToday () {
+      this.$refs.calendar.moveToToday()
+    },
+    onPrev () {
+      this.$refs.calendar.prev()
+    },
+    onNext () {
+      this.$refs.calendar.next()
+    },
+
+    onMoved (data) {
+      console.log('onMoved', data)
+    },
+    onChange (data) {
+      console.log('onChange', data)
+    },
+    onClickDate (data) {
+      console.log('onClickDate', data)
+    },
+    onClickTime (data) {
+      console.log('onClickTime', data)
+    },
+    onClickInterval (data) {
+      console.log('onClickInterval', data)
+    },
+    onClickHeadIntervals (data) {
+      console.log('onClickHeadIntervals', data)
+    },
+    onClickHeadDay (data) {
+      console.log('onClickHeadDay', data)
+    }
+  }
+})
+</script>
