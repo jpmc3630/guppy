ae7f58b273eb9a98b8750ef9022e48dd1d106c74
James McGlone <jamespmcglone@gmail.com>
2022-04-17 01:09:35 +0800

    Add QMonth component

--- quasar/src/pages/IndexPage.vue
+++ quasar/src/pages/IndexPage.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page class="flex flex-center">
+    <QMonth />
     <QAgenda />
   </q-page>
 </template>
@@ -7,11 +8,13 @@
 <script>
 import { defineComponent } from 'vue'
 import QAgenda from '@/components/QAgenda.vue'
+import QMonth from '@/components/QMonth.vue'
 
 export default defineComponent({
   name: 'IndexPage',
   components: {
-    QAgenda
+    QAgenda,
+    QMonth
   }
 })
 </script>
